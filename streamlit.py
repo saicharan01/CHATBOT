a519632769ae390b9ba051bd930b7bde107d1d4c
saicharan01 <137391888+saicharan01@users.noreply.github.com>
2023-08-11 12:47:41 +0530

    Update streamlit.py

--- streamlit.py
+++ streamlit.py
@@ -1,2 +1,19 @@
 import streamlit as st
-st.text('Hello')
+
+def main():
+    st.title("Snowflake Information Collector")
+    
+    # Collect user input
+    account = st.text_input("Snowflake Account URL (without https://):")
+    region = st.selectbox("Region:", ["us-east-1", "us-west-2", "eu-west-1"])  # Add more regions if needed
+    database = st.text_input("Database:")
+    
+    if st.button("Collect Info"):
+        st.write("Collected Information:")
+        st.write(f"Snowflake Account: {account}")
+        st.write(f"Region: {region}")
+        st.write(f"Database: {database}")
+        
+if __name__ == "__main__":
+    main()
+
