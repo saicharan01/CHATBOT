da842fac56518101ba7e680993ecdf0441125a54
saicharan01 <137391888+saicharan01@users.noreply.github.com>
2023-08-11 14:46:44 +0530

    Update streamlit.py

--- streamlit.py
+++ streamlit.py
@@ -1,5 +1,6 @@
 import streamlit as st
 import snowflake.connector
+import time
 
 def main():
     st.title("Snowflake Information Collector")
@@ -12,23 +13,34 @@ def main():
     password = st.text_input("Password:", type="password")
     
     if st.button("Collect Info"):
-        # Connect to Snowflake
-        conn = snowflake.connector.connect(
-            user=username,
-            password=password,
-            account=account,
-            region=region,
-            database=database
-        )
+        max_retries = 3
+        retry_delay = 5  # in seconds
         
-        st.write("Connected to Snowflake!")
-        st.write("Collected Information:")
-        st.write(f"Snowflake Account: {account}")
-        st.write(f"Region: {region}")
-        st.write(f"Database: {database}")
-        
-        # Don't forget to close the connection when done
-        conn.close()
-        
+        for attempt in range(1, max_retries + 1):
+            try:
+                # Connect to Snowflake
+                conn = snowflake.connector.connect(
+                    user=username,
+                    password=password,
+                    account=account,
+                    region=region,
+                    database=database
+                )
+                
+                st.write("Connected to Snowflake!")
+                st.write("Collected Information:")
+                st.write(f"Snowflake Account: {account}")
+                st.write(f"Region: {region}")
+                st.write(f"Database: {database}")
+                
+                # Don't forget to close the connection when done
+                conn.close()
+                break  # Successful connection, exit loop
+            except snowflake.connector.errors.DatabaseError as e:
+                st.write(f"Connection attempt {attempt} failed. Retrying...")
+                time.sleep(retry_delay)
+        else:
+            st.error("Failed to connect after multiple attempts. Please check your credentials and network.")
+
 if __name__ == "__main__":
     main()
