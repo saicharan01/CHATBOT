eae439ca4af643f019973d4dcf3786ef505de300
saicharan01 <137391888+saicharan01@users.noreply.github.com>
2023-08-16 11:05:10 +0530

    Update streamlit.py

--- streamlit.py
+++ streamlit.py
@@ -2,7 +2,7 @@ import streamlit as st
 import openai
 import snowflake.connector
 
-openai.api_key = "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"
+openai.api_key = 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
 
 def main():
     st.title("Snowflake Manual Connection and Chat")
